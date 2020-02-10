6a9e17d022fe909ff4a8a951d1eb60b0b3894aaa
SabaKhangholi <55306943+SabaKhangholi@users.noreply.github.com>
2020-02-09 22:57:44 -0800

    Update QueueT.h

--- QueueT.h
+++ QueueT.h
@@ -23,20 +23,48 @@ public:
 	// PRE: call copyQueue() helper method
 	// PARAM: qu = parameter QueueT object deep copy
 	// POST: creates deep copy of QueueT parameter
-	QueueT(const QueueT & qu);
+	QueueT(QueueT const &);
+	// PRE: call copyQueue() helper method
+	// PARAM: qu = parameter QueueT object deep copy
+	// POST: creates deep copy of QueueT parameter
 	~QueueT();
-	QueueT& operator=(const QueueT & qu);
+	// PRE: call copyQueue() helper method
+	// PARAM: qu = parameter QueueT object deep copy
+	// POST: creates deep copy of QueueT parameter
+	QueueT& operator=(QueueT const & qu);
+	// PRE: call copyQueue() helper method
+	// PARAM: qu = parameter QueueT object deep copy
+	// POST: creates deep copy of QueueT parameter
 	void enqueue(int value);
+	// PRE: call copyQueue() helper method
+	// PARAM: qu = parameter QueueT object deep copy
+	// POST: creates deep copy of QueueT parameter
 	int dequeue();
+	// PRE: call copyQueue() helper method
+	// PARAM: qu = parameter QueueT object deep copy
+	// POST: creates deep copy of QueueT parameter
 	void print();
+	// PRE: call copyQueue() helper method
+	// PARAM: qu = parameter QueueT object deep copy
+	// POST: creates deep copy of QueueT parameter
 	bool empty();
-	int size();
+	// PRE: call copyQueue() helper method
+	// PARAM: qu = parameter QueueT object deep copy
+	// POST: creates deep copy of QueueT parameter
+	int size();	
+	// PRE: call copyQueue() helper method
+	// PARAM: qu = parameter QueueT object deep copy
+	// POST: creates deep copy of QueueT parameter
 	void concatenate(QueueT & qu, int n);
+	// PRE: call copyQueue() helper method
+	// PARAM: qu = parameter QueueT object deep copy
+	// POST: creates deep copy of QueueT parameter
 	QueueT merge(const QueueT& qu);
 	//add getFront
 private:
 	NodeT* front;
 	NodeT* back;
 	int numValues;
-	void copyQueue(const QueueT & qu);
+	void copyQueue(QueueT const & qu);
+	void deleteQueue();
 };
