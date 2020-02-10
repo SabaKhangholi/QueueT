07297e701d8d8cb4afdf70acfba18c7a6bb1c849
SabaKhangholi <55306943+SabaKhangholi@users.noreply.github.com>
2020-02-09 22:58:11 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,37 @@ using namespace std;
 #include "QueueT.h"
 
 int main(){
-	QueueT q;
-	QueueT copy;
-	q.enqueue(4);
-	q.enqueue(5);
-	q.enqueue(6);
-	q.enqueue(7);
-	copy = q;
+	QueueT hi;
+	QueueT qu;
+	hi.enqueue(5);
+	hi.enqueue(4);
+	hi.enqueue(4);
+	
+	qu.enqueue(1);
+	qu.enqueue(2);
+	qu.enqueue(3);
+	
+	//hi.enqueue(6);
+	
+	QueueT Q(qu.merge(hi));
+	//Q = qu.merge(hi);
+	Q.print();
+	//Q(qu.merge(hi));
+	/*
+	qu.enqueue(5);
+	qu.enqueue(6);
+	qu.enqueue(7);
+	qu.enqueue(8);
+	*/
 	//q.print();
-	copy.print();
+	//Q.print();
+	//Q(qu.merge(q));
+	//mergeQ.print();
+	//mergeQ.enqueue(3);
+
+//	q.concatenate(qu, 1);
+//	q.print();
+//	cout<<"qu"<<endl;
+//	qu.print();
+	
 }
